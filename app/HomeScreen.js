25f827961d6336fe7a2345b556277aa5b017309e
Nazmul Idris <nazmul@fasterlap.com>
2017-04-11 14:48:08 -0700

    1) adding Styles 2) adding HomeScreen and changing index.android.js and index.ios.js to point to the same thing

---
+++ app/HomeScreen.js
@@ -0,0 +1,26 @@
+// @flow
+import React, {Component} from "react";
+
+import {AppRegistry, StatusBar, Text, View} from "react-native";
+
+import * as css from "./Styles";
+
+class WeatherApp extends Component {
+  render() {
+    const msg1 = `Weather App`;
+    const msg2 = `More coming soon!`;
+    
+    return (
+      <View style={css.homescreen.v_container}>
+        <StatusBar hidden={false} translucent={true} animated={true}
+                   barStyle={'light-content'} backgroundColor={'#455a64'}/>
+        <Text style={css.homescreen.heading}>{msg1}</Text>
+        <Text style={css.homescreen.body}>{msg2}</Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('WeatherApp', () => WeatherApp);
+
+export {WeatherApp}
---
+++ app/Styles.js
@@ -0,0 +1,46 @@
+// @flow
+
+import React from "react";
+import {StyleSheet} from "react-native";
+
+const homescreen = StyleSheet.create(
+  {
+    v_container: {
+      flex: 1,
+      flexDirection: 'column', // main axis
+      justifyContent: 'center', // main axis
+      alignItems: 'center', // cross axis
+      backgroundColor: '#d9d9d9',
+    },
+    heading: {
+      flex: -1, // shrink to min height & width if needed
+      fontSize: 20,
+      color: '#263238',
+      fontFamily: 'NotoSans-Bold', // more info https://goo.gl/7wYazn
+      margin: 8,
+    },
+    body: {
+      flex: -1,
+      margin: 8,
+      fontFamily: 'NotoSans-Regular', // more info https://goo.gl/7wYazn
+      fontSize: 12,
+      color: '#455a64',
+    },
+    h_container: {
+      flex: -1,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignSelf: 'stretch', // overrides container alignItems
+      backgroundColor: '#607d8b',
+      padding: 16,
+    },
+    icon: {
+      flex: -1,
+      margin: 8,
+      height: 100,
+      width: 75,
+      resizeMode: 'contain', //'cover' | 'contain'
+    },
+  });
+
+export {homescreen}
